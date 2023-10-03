0dae580bc3b2d862895be93c4671efe0f1961a44
MortenMink <bubbor123123@gmail.com>
2023-10-03 08:47:35 +0200

    Fixet bubblesort

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -68,32 +68,29 @@ def insertionSort(items):
                 break
     return items
 
-def bubbleSort (items):
-    items = items.copy()#laver kopi af items
+def bubbleSort(items):
+    items = items.copy()  # laver kopi af items
     n = len(items)
     swapped = False
+    for i in range(n - 1):  # lykken gentager sig selv, sidste tal i listen vender sig om
 
-    for i in range(n-1):#lykken gentager sig selv, sidste tal i listen vender sig oms
-
-        for j in range(0,n-i-1):#krydser array fra 0 til n-i-1
-
-            if items[j] > items[j+1]:#for lykke bytter tal hvis det er større end næste tal
+        for j in range(0, n - i - 1):  # krydser array fra 0 til n-i-1
+            if items[j] > items[j + 1]:  # for lykke bytter tal hvis det er større end næste tal
                 swapped = True
-                items[j], items[j+1] = items[j+1], items[j]#Bytter tal rundt
+                items[j], items[j + 1] = items[j + 1], items[j]  # Bytter tal rundt
 
-            if not swapped:
-                break
+        if not swapped:
+            break
     return items
 
 
-
 if __name__ == '__main__':
-    l = list(range(0, 5))
+    l = list(range(0, 20))
     lb = l.copy()
     for i in range(50):
         random.shuffle(lb)
         ## Kald den funktion, du vil teste
-        ls = mergeSort(lb,0, 4)
+        ls = insertionSort(lb)
         ## Kald den funktion, du vil teste
         if ls != l:
             print('Fejl! Algoritmen kan ikke sortere.')
